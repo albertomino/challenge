ebc694e11d2045df36cdc19f2ad692c67cb3331c
Ubuntu <ubuntu@cl1.local.net>
2017-10-20 16:26:19 +0000

    se agregan sitio a http_traffic_generator

--- http_https_traffic_generator.py
+++ http_https_traffic_generator.py
@@ -6,7 +6,8 @@ n = sys.argv[1]
 
 ## The urls used below depends on a dns or some "name to ip" solution like edit /etc/hosts file on linux.
 
-urls=['https://www.google.com', 'http://www.mercadolibre.com.ar', 'http://www.santanderrio.com', 'http://www.debian.org']
+urls=['https://www.google.com', 'http://www.mercadolibre.com.ar', 'http://www.santanderrio.com', 'http://www.debian.org', 'http://fernswa.myspecies.info', 'http://pngbirds.myspecies.info', 'http://nolinoideae.e-monocot.org', 'http://thymus.myspecies.info', 'http://chondrichthyes.myspecies.info', 'http://bormene.myspecies.info', 'http://pleistocenekokemushi.myspecies.in', 'http://dombeya.myspecies.info', 'http://aponogetonaceae.e-monocot.org', 'http://data.neomaps.org', 'http://aframomum.myspecies.info', 'http://itaxa.myspecies.info', 'http://crinoids.myspecies.info']
+
 
 # create the request object using funtions to determine how many packets or curls are desired using:
 # req = urllib2.Request(url)
@@ -17,12 +18,14 @@ def pcurl(n, urls):
    for url in urls:
     res = urllib2.urlopen(url)
     print res.read()
-    time.sleep(0.1)
+    time.sleep(0.5)
  else:
   while n > 0:
    for url in urls:
     res = urllib2.urlopen(url)
+    print 'curling %s' % url
     print res.read()
+    time.sleep(0.5)
     n = int(n)-1
 
 # make the request and print the results
