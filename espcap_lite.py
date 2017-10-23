ae1e1d87d87460bac6576dd61907815092927bf3
amino <alberto.mino@mecadolibre.com>
2017-10-23 13:13:17 -0300

    fix2 escaplite

--- espcap_lite.py
+++ espcap_lite.py
@@ -115,7 +115,7 @@ def main(nic, file, list, live):
         capture = pyshark.LiveCapture(nic, bpf_filter='not tcp port 22 and not tcp port 2220 and not tcp port 2240')
         dump_packets_to_dict(capture)
     elif file == None:
-        capture = pyshark.LiveCapture(nic, output_file='captura.pcap', bpf_filter='not tcp port 22 and not tcp port 2220')
+        capture = pyshark.LiveCapture(nic, output_file='captura.pcap', bpf_filter='not tcp port 22 and not tcp port 2220 and not tcp port 2240')
         dump_packets(capture)
 
 
