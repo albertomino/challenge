43277a45895c518c65b634d182dc9f077656b77f
amino <alberto.mino@mecadolibre.com>
2017-10-24 13:22:22 -0300

    se modifican pycurl y ntp-traffic

--- ntp-traffic.py
+++ ntp-traffic.py
@@ -2,16 +2,23 @@
 
 
 import ntplib
+import itertools
+import sys
 from time import ctime
 
 ntplist = ['0.us.pool.ntp.org', '1.us.pool.ntp.org', '2.us.pool.ntp.org', '3.us.pool.ntp.org']
 
 c = ntplib.NTPClient()
 
-for ntpserver in ntplist:
+n = int(sys.argv[1])
+
+for ntpserver in itertools.cycle(ntplist):
     try:
         response = c.request(ntpserver)
         print(ctime(response.tx_time))
     except Exception as e:
         pass
+    n -= 1
+    if n == 0:
+        break
 print 'Done!'
--- pycurl-http.py
+++ pycurl-http.py
@@ -2,6 +2,8 @@
 
 import requests
 import sys
+import time
+import itertools
 
 n = sys.argv[1]
 
@@ -10,6 +12,28 @@ urls=['http://www.google.com', 'http://www.mercadolibre.com.ar', 'http://www.san
 
 headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
 
-for url in urls:
- r = requests.post(url, headers=headers)
- print r
+#for url in urls:
+# r = requests.post(url, headers=headers)
+# print r
+
+def pcurl(n, urls):
+ if n == 'i':
+  while True:
+   for url in urls:
+    r = requests.post(url, headers=headers)
+    return r
+    time.sleep(0.5)
+ else:
+    for url in itertools.cycle(urls):
+     r = requests.post(url, headers=headers)
+     print 'curling %s' % url
+     print r
+     time.sleep(0.5)
+     n -= 1
+     print n
+     if n == 0:
+      break
+
+# make the request and print the results
+
+print  pcurl(n, urls)
